12c80b5a542906181fec3a04c3cc9b6eb54c982f
moonhj00 <violet.hj@gmail.com>
2014-04-16 13:52:45 +0000

    step3-4 related codes, April 16 morning

---
+++ projFocus/ceRNA/model/parseGslistFile.py
@@ -0,0 +1,12 @@
+import re
+def parseGslistFile(gene, file):
+    # smplist = [] 
+    with(open(file)) as f:
+        line = f.readline()
+        while line:
+            ptn = gene + "\t"
+            if re.match(ptn, line):
+                smps  = line.strip().split("\t",1)[1].split(";")
+                break 
+            line = f.readline()
+    return smps
