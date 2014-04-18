cc9e1c5667bb11e268c161ed533a3124302a7e44
moonhj00 <violet.hj@gmail.com>
2014-04-18 15:11:42 +0000

    Apri.18 2014 msmc updated with getMaxFreq, getMinSizeSets methods to select solution

--- projFocus/ceRNA/model/msmc.py
+++ projFocus/ceRNA/model/msmc.py
@@ -67,34 +67,6 @@ def updateSets(Sdict,Unew):
             Sout[k] = nv
     return Sout
 
-# def subSamplingMsmc(mutDict, mutDictInfo , alpha, maxIter = 10000): 
-#     ''' U should not be sorted '''
-#     out         = defaultdict(int)
-#     k           = int(alpha * len(mutDictInfo['mutGintSmpNum'] ) ) 
-#     i           = 1 
-#     for tempsample  in combination_sample(mutDictInfo['mutGintSmpNum'],k):  
-#     # for i in range(numIter):
-#         # tempsample     = random.sample(mutDictInfo['mutGintSmpNum'],k) 
-#         tempMutDict    = updateSets(mutDict, tempSample)
-#         tempResSets, _ = msmc(tempMutDict.values(), set(tempSample))
-#         # print tempmutdict.keys(), set(tempsample)
-#         # print tempressets
-#         tempResGSet  = []
-#         for gene, v in tempMutDict.items() :
-#             if v in tempressets:
-#                 tempResGSet.append(gene)   
-#                 tempResStr = ";".join(tempResGSet)
-#                 if out.get(tempResStr,''):
-#                     out[tempResStr][1] = out[tempResStr][1] + 1
-#                 else:
-#                     out[tempResStr] = [len(tempResGSet), 1]
-#                 print "iteration " + str(i) + " selected genes " + tempResStr    
-#             i = i + 1
-#             if i > maxIter:
-#                 break 
-#     out = sorted(out.items(),key=lambda x:x[1][0]) 
-#     return out 
-
 def findSsCutUp(alpha, csCutUp):
     i       = 1 
     sseff   = math.ceil(math.sqrt(csCutUp))
@@ -106,17 +78,17 @@ def findSsCutUp(alpha, csCutUp):
             ss = ss - 1 
             i = i + 1
         
-def genCombination(population, alpha, ssCutLow, csCutUp):
+def genCombination(population, alpha, sseffCutLow, csCutUp):
     ss      = len(population)
     sseff   = int(math.ceil( ss * alpha))
     combs   = scipy.misc.comb( ss, sseff)
-    # ssCutup = findSsCutUp(alpha, csCutUp) 
-    if sseff <= ssCutLow:
-        combSet = population
+    if sseff <= sseffCutLow:
+        combSet = [population]
     elif scipy.misc.comb(ss, sseff) > csCutUp:
-        # randPopu = random.sample(population, ssCutup)
-        combSet  = random.sample(combination_sample(population,\
-                                sseff), csCutUp)
+        ##----need to optimized
+        combSet  = random.sample(list(combination_sample(population,\
+                                sseff)), csCutUp)
+        
     else:
         combSet = combination_sample(population, sseff) 
     return combSet
@@ -140,50 +112,62 @@ def findAllSol(population, mutDict, \
         tempgeneset = '' 
         cntg        = 0 
         for gene, v in tempmutdict.items() :
-            if v in tempressets:
+            if set(v) in tempressets:
                 cntg = cntg + 1
-                tempgeneset = tempgeneset+";"+gene   
+                tempgeneset = tempgeneset + gene +";"  
 
         tempgval = resultD.get(tempgeneset, '')
         if cntg < gssCut:
             continue 
         if tempgval:
-            resultD['tempgeneset'] = [cntg, tempgval[1] + 1] 
+            resultD[tempgeneset] = [cntg, tempgval[1] + 1] 
         else:
-            resultD['tempgeneset'] = [cntg, 1] 
+            resultD[tempgeneset] = [cntg, 1] 
         if debug:
-            print tempmutdict.keys(), set(tempsample)
-            print tempressets
-            print "iteration " + str(i) + " selected " + str(cntg) + \
-                    " genes:\t" + tempgeneset
+            print "iteration " + str(i) +  "\tpopulation", tempsample, "\tselected " + str(cntg) + \
+                    " genes:", tempressets, "\tkey value:", tempgeneset
     return resultD
 
 #----optimizing solution
-
 def  getMinSizeSets(resultD):
-    while resultD:
-        gset    = result.pop()
-        ssMin   = 1000
-        valMax  = 0 
-        gRes    = []
-        for k, v in out.items():
-            if v[0] < ssMin :
-                ssMin = v[0]
-                gRes  = k  
-            elif v == ssMin:
-                gRes.append(k) 
-            else:
-                continue
-        return gRes 
+    ssMin   = 1000
+    gRes    = []
+    for k, v in resultD.items():
+        if v[0] < ssMin :
+            ssMin = v[0]
+            gRes  = k  
+        elif v == ssMin:
+            gRes.append(k) 
+        else:
+            continue
+    return gRes 
 
 def getMinSizeOverlapSets(result):
     return ''
 
+def getMaxFreq(resultD):
+    freqMax = -1 
+    gRes    = []
+    for k, v in resultD.items():
+        if  v[1] > freqMax:
+            freqMax = v[1]
+            gRes = k 
+        elif v[1] == freqMax:
+            gRes.append(k)
+        else:
+            continue
+    return gRes
+
 def selectSets(resultD, type = 'minsize'):
-    if   type == "minisize":
-        return getMinSizeSets(result)
-    elif type == "minsoverlap":
-        return getMinSizeOverlapSets(result) 
+    print type 
+    if   type == "minsize":
+        return getMinSizeSets(resultD)
+    elif type == "maxfreq":
+        return getMaxFreq(resultD) 
+    elif type == "minssOverlap":
+        return getMinSizeOverlapSets(resultD) 
+    else:
+        print "type not match"
 
 #------------------test
 def __test__():
@@ -221,8 +205,13 @@ def __testSample__():
     # print "whole set\t", list(genCombination(seq, 0.6, 4, 100))
     # print "get at most 5 set\t", list(genCombination(seq, 0.6, 2, 5))
 
-    print findAllSol(seq, seqSet, ssCutLow = 2, csCutUp = 100, debug = True) 
-    print findAllSol(seq, seqSet, alpha = 0.7, ssCutLow = 4, csCutUp = 100, debug = True) 
-    print findAllSol(seq, seqSet, alpha = 0.6, ssCutLow = 2, csCutUp = 7, debug = True) 
-__testSample__()
+    result  = findAllSol(seq, seqSet, ssCutLow = 2, csCutUp = 100, debug = True) 
+    print selectSets(result)
+    result  =  findAllSol(seq, seqSet, alpha = 0.6, ssCutLow = 2, csCutUp = 100, debug = True) 
+    print result, selectSets(result)
+    result  = findAllSol(seq, seqSet, alpha = 0.6, ssCutLow = 2, csCutUp = 7, debug = True) 
+    print result, selectSets(result)
+    print result, selectSets(result, type = "maxfreq")
+
+    __testSample__()
 
