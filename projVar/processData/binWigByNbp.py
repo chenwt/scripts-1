8788e000f0ca3508247dbc5ea17306917a4ae9f3
moonhj00 <violet.hj@gmail.com>
2014-08-08 19:41:47 +0000

    Aug 8 2014, updated with skipping headers ,handle variableStep line

--- projVar/processData/binWigByNbp.py
+++ projVar/processData/binWigByNbp.py
@@ -13,13 +13,14 @@ import sys,getopt
 
 argv = sys.argv[1:]
 usage = 'python ' + sys.argv[0] + '  -i <input wig file for one chrom> \
-    -b <bin size> -c < flush out cut size, lines>'
+    -b <bin size> -c < flush out cut size, lines> -s < comment line to skip, start with #>'
 example = 'python ' + sys.argv[0] + ' \
     -i /ifs/data/c2b2/ac_lab/jh3283/projMisc/varReg/data/genomicFeature/conservation/test/test.wig \
-    -b 10 -c 20 '
+    -b 10 -c 20 -s 6'
 
+nskip = 6
 try:
-    opts,args = getopt.getopt(argv,"hi:b:c:")
+    opts,args = getopt.getopt(argv,"hi:b:c:s:")
 except getopt.GetoptError as e: 
     print "ERROR", e
     print usage + "\n" + example 
@@ -35,53 +36,62 @@ for opt, arg in opts:
         n = int(arg)
     elif opt in ("-c"):
         flushCut = int(arg)
+    elif opt in ("-s"):
+        nskip = int(arg)
 
-outfile = inwig + "_" + str(n)  + ".bed" 
-
+##--func 
 def flushOut(outblock, outfh) :
     outfh.write("\n".join(outblock) + "\n")
     outfh.flush()
 
 
+outfile = inwig + "_" + str(n)  + ".bed" 
 header = ''
 outfh = open(outfile, 'aw') ## append to existing file
 
 with(open(inwig)) as f: 
-    for i in range(6):
-        header = header + f.readline()
+    if nskip > 0 :
+        for i in range(6):
+            header = header + f.readline()
     line = f.readline()
-    
-#     outfh.write(header + "\t" + line +  "chrom\tstart\tend\tvalue\n")
-    stepType, chrom, span = line.strip().split()
 
-    span = int(span.replace("span=","")); chrom = chrom.replace("chrom=","")
-    
-    line = f.readline()
     outblock = [] * flushCut
-    ps_prev, val_prev = line.strip().split()
-    ps_prev = int(ps_prev); val_prev = float(val_prev)
 
     while line:
+
+        if line[:12]=="variableStep" :
+            ## header line: output and reset
+            stepType, chrom, span = line.strip().split()
+            span = int(span.replace("span=","")); chrom = chrom.replace("chrom=","")
+
+            line = f.readline()
+            ps_prev, val_prev = line.strip().split()
+            ps_prev = int(ps_prev); val_prev = float(val_prev)
+    
+            if len(outblock) > 0 :
+                flushOut(outblock, outfh) 
+            outblock = [] * flushCut
+
+            line = f.readline()
+            continue
+
         ps_crt, val_crt = line.strip().split()
         ps_crt = int(ps_crt); val_crt = float(val_crt)
+
         if  ps_crt - ps_prev < n:
-            #within span
-            
-            #print ps_prev, ps_crt
+            #within new span
             val_prev = max(val_crt, val_prev)
-            #print "ifs"
             
         else:
-            # outside span
-            outblock.append("\t".join(map(str,[chrom, ps_prev, ps_crt, val_prev])))
+            # outside new span
+            outblock.append("\t".join(map(str,[chrom, ps_prev,\
+                                               min(ps_crt, ps_prev + n), val_prev])))
             
             if len(outblock) >= flushCut:
                 flushOut(outblock, outfh)
                 outblock = [] * flushCut
-                #print "output"
                 
             ps_prev = ps_crt; val_prev = val_crt 
-            #print "reset"
     
         line = f.readline()
         
