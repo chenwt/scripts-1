4bb712344bd6f676bb113af2674b7cd45aa598af
moonhj00 <violet.hj@gmail.com>
2014-08-22 22:59:47 +0000

    updated with tune.svm, using tuned parametered to run bootstrapping

--- projFocus/tf/model/step8-5_reg_tfCerna.r
+++ projFocus/tf/model/step8-5_reg_tfCerna.r
@@ -167,6 +167,51 @@ noseHeatmap = function(inputfile){
 
 svReg = function(inputfile) {
   # tgene = tail(unlist(strsplit(inputfile,"_")),1)
+  
+  data = read.csv2(inputfile, sep="\t", header=T,stringsAsFactors=F)
+  data = data[order(as.numeric(as.character(data$cTarExp))),]
+  allSmp = data$X; rownames(data) <- allSmp
+  
+  data = data[,colSums(data!=0)!=0]
+  
+  allFeatures = colnames(data)
+  ftr.cTarExp = allFeatures[grep("cTarExp", allFeatures)]
+  ftr.cTarCNV = allFeatures[grep("cTarCNV", allFeatures)]
+  ftr.cRegExp = allFeatures[grep("cRegExp", allFeatures)]
+  ftr.cRegCNV = allFeatures[grep("cRegCNV", allFeatures)]
+  ftr.cRegTFmut = allFeatures[grep("cRegTFmut", allFeatures)]
+  
+  if (length(ftr.cRegTFmut) == 0 ){
+    return(c(tgene, length(ftr.cRegCNV), 0, NROW(data), NA, NA))
+  }
+  
+  data[,c(ftr.cRegTFmut, ftr.cTarCNV, ftr.cRegCNV)] <- sign(apply(data[,c(ftr.cRegTFmut,ftr.cTarCNV, ftr.cRegCNV)],2, as.numeric))
+  data[,c(ftr.cTarExp, ftr.cRegExp)] <- apply(data[,c(ftr.cTarExp, ftr.cRegExp)], 2, as.numeric)
+  
+  data.mod1 = data.frame(as.matrix(data[,c(ftr.cTarExp, ftr.cTarCNV)]))
+  data.mod2 = data.frame(as.matrix(data[,c(ftr.cTarExp, ftr.cTarCNV, ftr.cRegCNV)]))
+  data.mod3 = data.frame(as.matrix(data[,c(ftr.cTarExp, ftr.cTarCNV, ftr.cRegCNV, ftr.cRegTFmut)]))
+  
+  require(e1071)
+  #   cost1 = 1; cost2 = 100 ; cost3 = 1000; gamma = 0.001
+  #   cost = 1000
+  #   mod1 = svm(data.mod1[,ftr.cTarExp] ~ ., data = data.frame(data.mod1), type="eps-regression",  kernel = "linear", cost = 1/length(ftr.cTarCNV), gamma = gamma, cross = 10)
+  #   mod2 = svm(data.mod2[,ftr.cTarExp] ~ ., data = data.frame(data.mod2), type="eps-regression",  kernel = "linear", cost = 1/length(c(ftr.cTarCNV,ftr.cRegCNV)), gamma = gamma, cross = 10)
+  #   mod3 = svm(data.mod3[,ftr.cTarExp] ~ ., data = data.frame(data.mod3), type="eps-regression",  kernel = "linear", cost = 1/length(c(ftr.cTarCNV,ftr.cRegCNV,ftr.cRegTFmut)), gamma = gamma, cross = 10)
+  #   
+  #   mod1 = svm(data.mod1[,ftr.cTarExp] ~ data.mod1[,-1], data = data.mod1, type="eps-regression",  kernel = "linear", cost = 1/length(ftr.cTarCNV), cross = 6)
+  #   mod2 = svm(data.mod2[,ftr.cTarExp] ~ ., data = data.frame(data.mod2), type="eps-regression",  kernel = "linear", cost = 1/length(c(ftr.cTarCNV,ftr.cRegCNV)), cross = 6)
+  #   mod3 = svm(data.mod3[,ftr.cTarExp] ~ ., data = data.frame(data.mod3), type="eps-regression",  kernel = "linear", cost = 1/length(c(ftr.cTarCNV,ftr.cRegCNV,ftr.cRegTFmut)), cross = 6)
+  mod = tune.svm(data.mod1[,1] ~ data.mod1[,-1] , data = data.mod1, range(type="eps-regression",  kernel = "linear", cost =10^c(0.01, 0.1, 1, 2, 3),cross = 6)); mod1 = mod$best.model
+  mod = tune.svm(data.mod2[,1] ~ as.matrix(data.mod2[,-1]) , data = data.mod2, range(type="eps-regression",  kernel = "linear", cost =10^c(0.01, 0.1, 1, 2, 3),cross = 6)); mod2 = mod$best.model
+  mod = tune.svm(data.mod3[,1] ~ as.matrix(data.mod3[,-1]) , data = data.mod3, range(type="eps-regression",  kernel = "linear", cost =10^c(0.01, 0.1, 1, 2, 3),cross = 6)); mod3 = mod$best.model
+  
+  return(list(mod1=mod1,mod2=mod2,mod3=mod3))
+  
+}
+
+svReg.boot = function(inputfile, mod1,mod2,mod3) {
+  # tgene = tail(unlist(strsplit(inputfile,"_")),1)
  
   data = read.csv2(inputfile, sep="\t", header=T,stringsAsFactors=F)
   data = data[order(as.numeric(as.character(data$cTarExp))),]
@@ -197,14 +242,21 @@ svReg = function(inputfile) {
   data.mod3 = as.matrix(data[,c(ftr.cTarExp, ftr.cTarCNV, ftr.cRegCNV, ftr.cRegTFmut)])
   
   require(e1071)
-  cost1 = 1; cost2 = 100 ; cost3 = 1000; gamma = 0.001
-  mod1 = svm(data.mod1[,ftr.cTarExp] ~ ., data = data.frame(data.mod1), type="eps-regression",  kernel = "polynomial", cost = cost1, gamma = gamma, cross = 10)
-  mod2 = svm(data.mod2[,ftr.cTarExp] ~ ., data = data.frame(data.mod2), type="eps-regression",  kernel = "polynomial", cost = cost2, gamma = gamma, cross = 10)
-  mod3 = svm(data.mod3[,ftr.cTarExp] ~ ., data = data.frame(data.mod3), type="eps-regression",  kernel = "polynomial", cost = cost3, gamma = gamma, cross = 10)
+  allkernels = c("linear", "polynomial", "radial","sigmoid")
+  mod1 = svm(data.mod1[,1] ~ data.mod1[,-1] , data = data.mod1, type="eps-regression",  kernel = allkernels[mod1$kernel], cost =mod1$cost, gamma=mod1$gamma, cross =10)
+  mod2 = svm(data.mod2[,1] ~ data.mod2[,-1] , data = data.mod2, type="eps-regression",  kernel = allkernels[mod2$kernel], cost =mod2$cost, gamma=mod2$gamma, cross =10)
+  mod3 = svm(data.mod3[,1] ~ data.mod3[,-1] , data = data.mod3, type="eps-regression",  kernel = allkernels[mod3$kernel], cost =mod3$cost, gamma=mod3$gamma, cross =10)
   
-  rss1 = sum(mod1$residuals)^2 ; rss2 = sum(mod2$residuals)^2; rss3 = sum(mod3$residuals)^2; 
   p1 = NCOL(data.mod1) -1 ; p2 = NCOL(data.mod2) -1 ; p3 = NCOL(data.mod3) - 1
   n = length(allSmp)
+  
+  rss1 = sum(mod1$residuals^2) ; rss2 = sum(mod2$residuals^2); rss3 = sum(mod3$residuals^2); 
+  sst = sum((data.mod1[,1]-mean(data.mod1[,1]))^2)
+  mod1$tot.MSE=1/n * sum((data.mod1[,1]-mod1$fitted)^2); mod1$scorrcoeff = (1- rss1/sst)
+  mod2$tot.MSE=1/n * sum((data.mod2[,1]-mod2$fitted)^2); mod2$scorrcoeff = (1- rss2/sst)
+  mod3$tot.MSE=1/n * sum((data.mod3[,1]-mod3$fitted)^2); mod3$scorrcoeff = (1- rss3/sst)
+
+
   
   ##alternative hypo: mod2 is bettern than mod1, rss1 is smaller than rss2
   out = c(tgene, mod1.mse=mod1$tot.MSE, mod1.r2 = mod1$scorrcoeff,
@@ -218,73 +270,47 @@ svReg = function(inputfile) {
 
 }
 
+
+plotMSEDensity = function(data){
+  data <- data.frame(resDF)
+  data[,-1] <- apply(data[,-1], 2, as.numeric)
+  ymax = max(density(as.numeric(data$mod1.mse))$y, density(as.numeric(data$mod2.mse))$y, density(as.numeric(data$mod3.mse))$y)
+  xmax = max(density(as.numeric(data$mod1.mse))$x, density(as.numeric(data$mod2.mse))$x, density(as.numeric(data$mod3.mse))$x)
+  xmin = min(density(as.numeric(data$mod1.mse))$x, density(as.numeric(data$mod2.mse))$x, density(as.numeric(data$mod3.mse))$x)
+  
+  plot(0, type="n", ylab = "density", xlab="",xlim = c(xmin,xmax),
+       ylim=c(0,ymax),
+       main = paste(tgene, "MSE"))
+  
+  lines(density(as.numeric(data$mod1.mse)), col= '#FF9900', lwd = 2)
+  lines(density(as.numeric(data$mod2.mse)), col = '#006633', lwd=2) 
+  lines(density(as.numeric(data$mod3.mse)), col = '#990033', lwd=2) 
+  
+  legend('topright',legend=c("model1", "model2", "model3"),lty=1, 
+         col=c("#FF9900","#006633","#990033"), bty ='n', cex = .75, lwd=2)
+  
+}
+
 ####---test----
-# nboot=1000
-# tgene = 'EN2'
+#  nboot=10
+#  tgene = 'AFF3'
 ###----main-------
 require(scales)
 inputDir = paste(rootd, "/DATA/projFocus/result/07152014/reg/tfCerna/data" , sep="")
 # inputfileLst = paste(inputDir, "/", list.files(inputDir), sep="")
 
-# tgene ="MBNL3"
 inputfile = paste(inputDir, '/', 'input_', tgene, sep="")
-# output  = paste(rootd, "/DATA/projFocus/result/07152014/reg/tfCerna/result/runAug22/result_", sep="")
-
-svReg_dev = function(inputfile){
-  data = read.csv2(inputfile, sep="\t", header=T,stringsAsFactors=F)
-  data = data[order(as.numeric(as.character(data$cTarExp))),]
-  allSmp = data$X; rownames(data) <- allSmp
-  
-   ### bootstrapping 
-  allSmp = sample(allSmp, replace = T)
-  data = data[allSmp, ] 
-  
-  data = data[,colSums(data!=0)!=0]
-  
-  allFeatures = colnames(data)
-  ftr.cTarExp = allFeatures[grep("cTarExp", allFeatures)]
-  ftr.cTarCNV = allFeatures[grep("cTarCNV", allFeatures)]
-  ftr.cRegExp = allFeatures[grep("cRegExp", allFeatures)]
-  ftr.cRegCNV = allFeatures[grep("cRegCNV", allFeatures)]
-  ftr.cRegTFmut = allFeatures[grep("cRegTFmut", allFeatures)]
-  
-  if (length(ftr.cRegTFmut) == 0 ){
-    return(c(tgene, length(ftr.cRegCNV), 0, NROW(data), NA, NA))
-  }
-  
-  data[,c(ftr.cRegTFmut, ftr.cTarCNV, ftr.cRegCNV)] <- sign(apply(data[,c(ftr.cRegTFmut,ftr.cTarCNV, ftr.cRegCNV)],2, as.numeric))
-  data[,c(ftr.cTarExp, ftr.cRegExp)] <- apply(data[,c(ftr.cTarExp, ftr.cRegExp)], 2, as.numeric)
-  
-  
-  data.mod1 = as.matrix(data[,c(ftr.cTarExp, ftr.cTarCNV)])
-  data.mod2 = as.matrix(data[,c(ftr.cTarExp, ftr.cTarCNV, ftr.cRegCNV)])
-  data.mod3 = as.matrix(data[,c(ftr.cTarExp, ftr.cTarCNV, ftr.cRegCNV, ftr.cRegTFmut)])
-  
-  require(e1071)
-  cost1 = 1; cost2 = 100 ; cost3 = 1000; gamma = 0.001
-  mod1 = svm(data.mod1[,ftr.cTarExp] ~ ., data = data.frame(data.mod1), type="eps-regression",  kernel = "polynomial", cost = cost1, gamma = gamma, cross = 10)
-  mod2 = svm(data.mod2[,ftr.cTarExp] ~ ., data = data.frame(data.mod2), type="eps-regression",  kernel = "polynomial", cost = cost2, gamma = gamma, cross = 10)
-  mod3 = svm(data.mod3[,ftr.cTarExp] ~ ., data = data.frame(data.mod2), type="eps-regression",  kernel = "polynomial", cost = cost3, gamma = gamma, cross = 10)
-  
-  rss1 = sum(mod1$residuals)^2 ; rss2 = sum(mod2$residuals)^2; rss3 = sum(mod3$residuals)^2; 
-  p1 = NCOL(data.mod1) -1 ; p2 = NCOL(data.mod2) -1 ; p3 = NCOL(data.mod3) - 1
-  n = length(unique(allSmp))
-  
-  ##alternative hypo: mod2 is bettern than mod1, rss1 is smaller than rss2
-   out = c(tgene, mod1.mse=mod1$tot.MSE, mod1.r2 = mod1$scorrcoeff,
-	  mod2.mse=mod2$tot.MSE, mod2.r2=mod2$scorrcoeff,
-	  mod3.mse=mod3$tot.MSE, mod3.r2 = mod3$scorrcoeff,
-	  rss1=rss1, rss2=rss2, rss3 = rss3,
-	  cntSelfCNV = p1, cntCregCNV=p2 - p1, cntMut=p3-p2,  cntSmp=n)
-  # print(out)
-  cat(sprintf("cv.svm> mse, r2 = %5.3f %5.3f\n", mod1$tot.MSE, mod1$scorrcoeff, mod2$tot.MSE, mod2$scorrcoeff))
-}
-
+output  = paste(rootd, "/DATA/projFocus/result/07152014/reg/tfCerna/result/runAug22/result_", sep="")
+ 
 print(inputfile)
-# resDF = sapply(inputfileLst,svReg)
-resDF = sapply(rep(inputfile, nboot), svReg)
+tune.mods = svReg(inputfile)
+resDF = sapply(rep(inputfile, nboot), function(x){svReg.boot(x,tune.mods$mod1,tune.mods$mod2,tune.mods$mod3)})
+
 resDF = t(resDF)
 rownames(resDF)  <- paste(tgene, 1:nboot, sep="_")
-
-write.table(resDF, paste(output, tgene ,sep=""), col.names=F, quote=F, sep="\t" )
+header = c("tgene", "mod1.mse", "mod1.r2", "mod2.mse", "mod2.r2",
+                  "mod3.mse", "mod3.r2" ,  "rss1", "rss2", "rss3" ,
+                 "cntSelfCNV", "cntCregCNV", "cntMut",  "cntSmp")
+colnames(resDF) = header
+write.table(resDF, paste(output, tgene ,sep=""), col.names=T, quote=F, sep="\t")
 
